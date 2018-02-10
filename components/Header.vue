a2beccf0aab0e66869dd67d6a455aa50f210ff43
Masahiko Sato <sato_m@mediaxis.jp>
2018-02-10 22:51:53 +0900

    add header

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav class="level">
+      <p class="level-item has-text-centered">
+        <img src="~/assets/img/logo.png" alt="logo" />
+      </p>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+nav {
+  padding: 10px;
+}
+</style>
