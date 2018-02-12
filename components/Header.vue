474201638b44783c86ff82aaf9382c79625cad9b
Masahiko Sato <sato_m@mediaxis.jp>
2018-02-12 21:53:24 +0900

    add add button to the header

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,27 @@
 <template>
   <header>
-    <nav class="level">
-      <p class="level-item has-text-centered">
-        <img src="~/assets/img/logo.png" alt="logo" />
-      </p>
-
-      <b-field class="level-right">
-        <b-input placeholder="Search..."
-                 type="search"
-                 icon="search">
-        </b-input>
-      </b-field>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="~/assets/img/logo.png" alt="logo" />
+        </a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <b-input placeholder="Search..."
+                     type="search"
+                     icon="search">
+            </b-input>
+            <a class="button">
+              <b-icon
+                icon="sync-alt"
+                custom-class="fa-plus">
+              </b-icon>
+            </a>
+          </div>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
-
-<style scoped>
-nav {
-  padding: 10px;
-}
-</style>
